7f04eb4b4bba2171a3d646416b203318305737d5
Aleksey Evdokimov <izzzi667@gmail.com>
2021-11-26 21:14:51 +0300

    Fix russian word ending

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from "react";
+import getNumEnding from "./getNumEnding";
 
 const Timer =(props) => { 	
 	return (<span>
 	<h1 className="mb-4 display-3">До нового года осталость</h1>
-	<h2 className="mb-4 display-3">{props.days} дней, {props.hours} часов {props.minutes} минут {props.seconds} секунд </h2></span>);
+	<h2 className="mb-4 display-3">
+		{props.days} {getNumEnding(props.days,['день','дня','дней'])}{' '}  
+		{props.hours} {getNumEnding(props.hours,['час','часа','часов'])}{' '}  
+		{props.minutes} {getNumEnding(props.hours,['минута','минуты','минут'])}{' '}   
+		{props.seconds}  {getNumEnding(props.seconds,['секуда','секунды','секунд'])} 
+	</h2></span>);
 	
 }
 
