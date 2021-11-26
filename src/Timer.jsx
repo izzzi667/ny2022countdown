b9bcec5c3897da3e192e5d3c3023c4a541a8af83
Aleksey Evdokimov <izzzi667@gmail.com>
2021-11-26 22:15:47 +0300

    Add New Year message

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import getNumEnding from "./getNumEnding";
 
 const Timer =(props) => { 	
+	if(props.days<0) return <h1 className="mb-4 display-1">С Новым Годом!</h1>
 	return (<span>
 	<h1 className="mb-4 display-3">До нового года осталость</h1>
 	<h2 className="mb-4 display-3">
