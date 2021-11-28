58fea78988c6e29cae0a22ebe1d15f125655628b
Aleksey Evdokimov <izzzi667@gmail.com>
2021-11-28 11:55:10 +0300

    Refactoring - Gift moved to single file

---
+++ src/Gift.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { Col, Modal, Button } from "react-bootstrap";
+import activeGift from './static/active.png'
+import inActiveGift from './static/inactive.png'
+import style from './Gift.module.css'
+import card from './static/card.jpg'
+
+const Gift = (props) =>
+{
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    function createMarkup() {
+        return {__html: props.gift};
+    }
+
+    if (!props.status){
+        return(<Col><img src={inActiveGift} onClick={handleShow}/><br />{props.num} декабря</Col>)
+    }
+
+    return (<>
+        <Col><img className={style.gift} src={activeGift} onClick={handleShow}/><br />{props.num} декабря</Col>
+        <Modal 
+          show={show} 
+          onHide={handleClose}       
+          size="xl"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered>
+          <Modal.Body>
+            <Col className='mh-100 py-5 text-center' style={{backgroundImage: `url(${card})`, height: '80vh'}}>
+              <h1 class="display-3">Сюрприз за {props.num} день</h1>
+              <div dangerouslySetInnerHTML={createMarkup()} /><br /><br />
+              <Button variant="secondary" onClick={handleClose}>
+              Да!
+              </Button>          
+            </Col>
+          </Modal.Body>
+        </Modal>
+        </>
+    );
+}
+
+export default Gift;
