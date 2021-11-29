5a084da9dce84e680501a4fd771c056fd0aa0c3b
Aleksey Evdokimov <izzzi667@gmail.com>
2021-11-29 20:16:32 +0300

    Add four gift images

--- src/Gift.jsx
+++ src/Gift.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { Col, Modal, Button } from "react-bootstrap";
-import activeGift from './static/active.png'
+import activeGift1 from './static/active1.png'
+import activeGift2 from './static/active2.png'
+import activeGift3 from './static/active3.png'
+import activeGift4 from './static/active4.png'
 import inActiveGift from './static/inactive.png'
 import style from './Gift.module.css'
 import card from './static/card.jpg'
@@ -19,8 +22,25 @@ const Gift = (props) =>
         return(<Col><img src={inActiveGift} onClick={handleShow}/><br />{props.num} декабря</Col>)
     }
 
+    let imgObject=null;
+    switch(props.imgCount) {
+      case 1:
+        imgObject=activeGift1;
+        break;
+      case 2:
+        imgObject=activeGift2;
+        break;
+      case 3:
+        imgObject=activeGift3;
+        break;        
+      case 4:
+        imgObject=activeGift4;
+        break;   
+    }
+
+
     return (<>
-        <Col><img className={style.gift} src={activeGift} onClick={handleShow}/><br />{props.num} декабря</Col>
+        <Col><img className={style.gift} src={imgObject} onClick={handleShow}/><br />{props.num} декабря</Col>
         <Modal 
           show={show} 
           onHide={handleClose}       
--- src/GiftList.jsx
+++ src/GiftList.jsx
@@ -1,59 +1,33 @@
 import React, { useEffect, useState } from "react";
-import { Col, Row, Modal, Button } from "react-bootstrap";
-import activeGift from './static/active.png'
-import inActiveGift from './static/inactive.png'
-import style from './Gift.module.css'
-import card from './static/card.jpg'
+import { Row } from "react-bootstrap";
+import Gift from "./Gift";
+
 
 const GiftList = (props) =>
 {
     const [giftList, setGiftList] = useState([]);
     useEffect(() => {
       let giftArray = [];
+      let imgCount=1;
       for(let i=1;i<=31;i++){
-        giftArray.push(<Gift key={'gift'+i} num={i} status={-31+i+props.days<=0?true:false} gift={props.gifts.get(i)}/>)
+
+        giftArray.push(
+        <Gift 
+          key={'gift'+i} 
+          num={i} 
+          status={-31+i+props.days<=0?true:false} 
+          gift={props.gifts.get(i)}
+          imgCount={imgCount}
+        />)
+        imgCount++;
+        if(imgCount>4) imgCount=1;
+          
       }      
       setGiftList(giftArray);
     }, [props.days])
     
     return(
         <Row>{giftList}</Row>
-    );
-}
-
-const Gift = (props) =>
-{
-    const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-
-    function createMarkup() {
-        return {__html: props.gift};
-    }
-
-    if (!props.status){
-        return(<Col><img src={inActiveGift} onClick={handleShow}/><br />{props.num} декабря</Col>)
-    }
-
-    return (<>
-        <Col><img className={style.gift} src={activeGift} onClick={handleShow}/><br />{props.num} декабря</Col>
-        <Modal 
-          show={show} 
-          onHide={handleClose}       
-          size="xl"
-          aria-labelledby="contained-modal-title-vcenter"
-          centered>
-          <Modal.Body>
-            <Col className='mh-100 py-5 text-center' style={{backgroundImage: `url(${card})`, height: '80vh'}}>
-              <h1 class="display-3">Сюрприз за {props.num} день</h1>
-              <div dangerouslySetInnerHTML={createMarkup()} /><br /><br />
-              <Button variant="secondary" onClick={handleClose}>
-              Да!
-              </Button>          
-            </Col>
-          </Modal.Body>
-        </Modal>
-        </>
     );
 }
 
